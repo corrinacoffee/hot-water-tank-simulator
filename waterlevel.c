dac6b8d9db0515921e05fa07a02f067fcdfb4281
Corrina Del Greco <corrinadelgreco@gmail.com>
2021-04-26 08:06:46 -0400

    Update waterlevel.c

--- waterlevel.c
+++ waterlevel.c
@@ -1,20 +1,23 @@
+#include <stdio.h>
 #include <stdbool.h>
 
 // true if open, false if closed
-bool inValve1 = false; // req 8
+bool inValve1 = false; // req 8: "The system shall have two inlet valves."
 bool inValve2 = false;
-bool outValve = false; // req 9
+bool outValve = false; // req 9: "The system shall have one outlet valve."
 
 // in liters
+// sensor 1 is lowest, 4 is highest
 int waterLevel = 0;
-int sensor1WaterLevel = 10; // req 12
+int sensor1WaterLevel = 10; // req 12: "The system shall have four water level sensors."
 int sensor2WaterLevel = 20;
 int sensor3WaterLevel = 30;
 int sensor4WaterLevel = 40;
-int MAX_WATER_LEVEL = 50; // req 23
+int MAX_WATER_LEVEL = 50; // req 23: "The volume of the tank shall be 50 L."
 
+// req 10: "Water shall enter the tank if either inlet valve is open."
+// req 11: "Water shall enter the tank faster when both inlets are open than when one is open."
 void updateWaterLevel() {
-  // req 11: "Water shall enter the tank faster when both inlets are open than when one is open."
   if (inValve1 && inValve2) {
     waterLevel = waterLevel+2;
   }
@@ -27,10 +30,46 @@ void updateWaterLevel() {
   if (outValve) {
     waterLevel--;
   }
-  
-  // req 14: "The system shall open the outlet valve when the water level reaches the highest sensor."
+}
+
+// req 14: "The system shall open the outlet valve when the water level reaches the highest sensor."
+void updateOutValve() {
   if (waterLevel >= sensor4WaterLevel) {
     outValve = true;
+  }
+}
+
+// req 12: "The system shall indicate when the water level reaches a certain sensor."
+int reachedWaterLevelSensor() {
+  if (waterLevel >= sensor4WaterLevel) {
+    return 4;
+  } else if (waterLevel >= sensor3WaterLevel) {
+    return 3;
+  } else if (waterLevel >= sensor2WaterLevel) {
+    return 2;
+  } else if (waterLevel >= sensor1WaterLevel) {
+    return 1;
+  } else {
+    return 0;
+  }
+}
+  
+// param open true if open, false if close
+void setInValve1Open(bool open) {
+  inValve1 = open;
+}
+
+// param open true if open, false if close
+void setInValve2Open(bool open) {
+  inValve1 = open;
+}
+
+// param open true if open, false if close
+void setOutValveOpen(bool open) {
+  if (waterLevel >= sensor4WaterLevel) {
+    outValve = true;
+  } else {
+    outValve = open;
   }
 }
 
@@ -54,4 +93,3 @@ void setWaterLevelSensor(int sensorNumber, int newWaterLevel) {
         sensor4WaterLevel = newWaterLevel;
       }
   }
-}
