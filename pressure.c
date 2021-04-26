0262660de42bbd430b2873fb9d2542dffec09aa2
Corrina Del Greco <corrinadelgreco@gmail.com>
2021-04-26 09:25:49 -0400

    Update pressure.c

--- pressure.c
+++ pressure.c
@@ -11,7 +11,8 @@ int pressure = 25;
 int DEFAULT_MAX_TEMP = 100; // req 27: "The default maximum temperature shall be 100°C."
 int pressureTempRange = 10; // req 21: "The pressure shall increase faster when then temperature is within a range of 100°C" user defined req 33: "10°C"
 
-// TODO need to retrieve 
+// TODO use real time construct to retreieve water level and temperature
+// TODO use real time construct to initiate action
 void updatePressure() {
   // req 20: "The pressure shall increase when the water level surpasses the second to highest water level sensor."
   if (waterLevel > sensor3WaterLevel) {
@@ -23,6 +24,7 @@ void updatePressure() {
   }
 }
 
+// TODO use real time construct to initiate action
 // req 22: "The system shall open the outlet valve when the pressure reaches the critical pressure of 50 psi (344 kPa)."
 void pressureCheck() {
   if (pressure >= CRIT_PRESSURE) {
