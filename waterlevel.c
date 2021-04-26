c5a736dfdbe71124b95cb5547896d2cf780bfded
Corrina Del Greco <corrinadelgreco@gmail.com>
2021-04-26 08:47:42 -0400

    Update waterlevel.c

--- waterlevel.c
+++ waterlevel.c
@@ -55,25 +55,31 @@ int reachedWaterLevelSensor() {
 }
 
 // req 29: "The system shall allow moving a water level sensor."
-void setWaterLevelSensor(int sensorNumber, int newWaterLevel) {
+int setWaterLevelSensor(int sensorNumber, int newWaterLevel) {
+  retVal = 0;
   switch(sensorNumber) {
     case 1:
       if (newWaterLevel > 0 && newWaterLevel < sensor2WaterLevel) {
         sensor1WaterLevel = newWaterLevel;
+        retVal = 1;
       }
     case 2:
       if (newWaterLevel > sensor1WaterLevel && newWaterLevel < sensor3WaterLevel) {
         sensor2WaterLevel = newWaterLevel;
+        retVal = 1;
       }
     case 3:
       if (newWaterLevel > sensor2WaterLevel && newWaterLevel < sensor4WaterLevel) {
         sensor3WaterLevel = newWaterLevel;
+        retVal = 1;
       }
     case 4:
       if (newWaterLevel > sensor3WaterLevel && newWaterLevel < MAX_WATER_LEVEL) {
         sensor4WaterLevel = newWaterLevel;
+        retVal = 1;
       }
   }
+  return retVal;
 }
 
 // param open true if open, false if close
