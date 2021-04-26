3d3d5022e9da282a7d73bfcebc1b22622810ba28
Corrina Del Greco <corrinadelgreco@gmail.com>
2021-04-26 09:44:41 -0400

    Update pressure.c

--- pressure.c
+++ pressure.c
@@ -7,9 +7,9 @@
 int MIN_NOM_OP_PRESSURE = 20; // req 24: "The minimum nominal operating pressure shall be 20 psi (130 kPa)."
 int MIN_NOM_OP_PRESSURE = 30; // req 25: "The maximum nominal operating pressure shall be 30 psi (206 kPa)."
 int CRIT_PRESSURE = 50; // req 26: "The critical pressure shall be 50 psi (344 kPa)."
-int pressure = 25;
-int DEFAULT_MAX_TEMP = 100; // req 27: "The default maximum temperature shall be 100°C."
-int pressureTempRange = 10; // req 21: "The pressure shall increase faster when then temperature is within a range of 100°C" user defined req 33: "10°C"
+int DEFAULT_MAX_TEMP = 100; // FIXME? in temperature module too but needed here // req 27: "The default maximum temperature shall be 100°C."
+int PRESSURE_TEMP_RANGE = 10; // req 21: "The pressure shall increase faster when then temperature is within a range of 100°C" user defined req 33: "10°C"
+int pressureSensor = 25;
 
 // TODO use real time construct to know waterLevel from waterlevel module
 // TODO use real time construct to initiate action
@@ -20,11 +20,11 @@ void updatePressureBasedOnWaterLevel() {
   }
 }
 
-// TODO use real time construct to know temp from temperature module
+// TODO use real time construct to know tempSensor from temperature module
 // TODO use real time construct to initiate action
 void updatePressureBasedOnTemperate() {
   // req 21: "The pressure shall increase faster when then temperature is within a range of 100°C."
-  if (temp > DEFAULT_MAX_TEMP + pressureTempRange) {
+  if (tempSensor > DEFAULT_MAX_TEMP + pressureTempRange) {
     pressure++;
   }
 }
@@ -33,7 +33,7 @@ void updatePressureBasedOnTemperate() {
 // TODO use real time construct to initiate action
 // req 22: "The system shall open the outlet valve when the pressure reaches the critical pressure of 50 psi (344 kPa)."
 void pressureSensorControlOutValve() {
-  if (pressure >= CRIT_PRESSURE) {
+  if (pressureSensor >= CRIT_PRESSURE) {
     outValve = true;
   }
 }
