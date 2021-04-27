08d4447307da61fda1f992e88ff8ac92f405ca14
Corrina Del Greco <corrinadelgreco@gmail.com>
2021-04-27 18:44:11 -0400

    Add files via upload

---
+++ waterlevel.h
@@ -0,0 +1,28 @@
+#ifndef __WATERLEVEL_H
+#define __WATERLEVEL_H
+
+/* Includes */
+
+/* Definitions and Macros */
+#define MAX_WATER_LEVEL 50	// req 23: "The volume of the tank shall be 50 L."
+
+/* Type, enum, and struct definitions */
+typedef enum {
+	WATER_SENSOR_NONE,
+	WATER_SENSOR_LOW,
+	WATER_SENSOR_LOWMID,
+	WATER_SENSOR_MIDHIGH,
+	WATER_SENSOR_HIGH
+} water_sensor_level_t;
+
+typedef struct {
+	water_sensor_level_t current_sensor;
+	int water_level;
+} water_sensor_t;
+
+/* Function prototypes */
+void WATER_Init(void);
+
+water_sensor_level_t WATER_RequestLevel(void);
+
+#endif
