caf38b32145beff7709e1d9999caf2c368c9dfc8
Corrina Del Greco <corrinadelgreco@gmail.com>
2021-04-27 19:26:04 -0400

    Update main.c

--- main.c
+++ main.c
@@ -2,6 +2,7 @@
 #include <sysLib.h>
 #include <tickLib.h>
 
+#include "commondefs.h"
 #include "waterlevel.h"
 #include "temperature.h"
 
@@ -13,10 +14,10 @@ static bool heater = false;
 // TODO use this everywhere
 // req 6: "The system shall capture/record/display the system events and actions."
 // req 5: "The system shall use time-stamping for identifying time of events (both ticks and sec/nsec)."
-void record(char *msg) {
+void record(char *fmt) {
   int timeInTicks = tickGet();
   int timeInSec = sysClkRateGet() / timeInTicks;
-  printf("%d %d %s", timeInTicks, timeInSec, msg); // TODO format nicely
+  printf("%d %d %s", timeInTicks, timeInSec, fmt); // TODO format nicely
 }
 
 int main(void) {
