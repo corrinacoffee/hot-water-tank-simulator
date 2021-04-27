d3fc870d62c60648ab3638770803d74cdc452bdd
Corrina Del Greco <corrinadelgreco@gmail.com>
2021-04-27 18:42:19 -0400

    Update and rename manager.c to main.c

---
+++ main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <sysLib.h>
+#include <tickLib.h>
+
+#include "waterlevel.h"
+#include "temperature.h"
+
+// idea: use mutex semaphore so no one can turn the heater on
+// until water has cooled to the desired temp (the mutex is released)
+// req 15: "The system shall have a water heater."
+static bool heater = false;
+
+// TODO use this everywhere
+// req 6: "The system shall capture/record/display the system events and actions."
+// req 5: "The system shall use time-stamping for identifying time of events (both ticks and sec/nsec)."
+void record(char *msg) {
+  int timeInTicks = tickGet();
+  int timeInSec = sysClkRateGet() / timeInTicks;
+  printf("%d %d %s", timeInTicks, timeInSec, msg); // TODO format nicely
+}
+
+int main(void) {
+	TEMP_Init();
+	WATER_Init();
+}
