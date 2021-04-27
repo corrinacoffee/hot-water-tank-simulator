fc607206f24856e9744e8642efed54848100f1b7
Corrina Del Greco <corrinadelgreco@gmail.com>
2021-04-27 18:43:38 -0400

    Add files via upload

---
+++ commondefs.h
@@ -0,0 +1,14 @@
+#ifndef __COMMONDEFS_H
+#define __COMMONDEFS_H
+
+/* Includes */
+#include <stdint.h>
+
+/* Definitions and Macros */
+#define true ((uint8_t)1)
+#define false ((uint8_t)0)
+
+/* Type, enumerator, and structure definitions */
+typedef uint8_t bool;
+
+#endif
