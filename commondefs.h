21ce77b53ad14d37447d06551d7944c1937204b4
Corrina Del Greco <corrinadelgreco@gmail.com>
2021-04-29 20:34:33 -0400

    Update commondefs.h

--- commondefs.h
+++ commondefs.h
@@ -3,16 +3,27 @@
 
 /* Includes */
 #include <stdint.h>
+#include <tickLib.h>
+#include <sysLib.h>
 
 /* Definitions and Macros */
-#define true ((uint8_t)1)
-#define false ((uint8_t)0)
+#define TIME_TICKS 		(tickGet())
+#define TIME_SECS		(tickGet() / sysClkRateGet())
 
-#define MESSAGE_SIZE sizeof(message_struct_t)
-#define MESSAGE_Q_SIZE 50
+#define true 			((uint8_t)1)
+#define false 			((uint8_t)0)
+
+#define MESSAGE_SIZE 	sizeof(message_struct_t)
+#define MESSAGE_Q_SIZE 	50
+#define MESSAGE_STRING_SIZE 256
 
 /* Type, enumerator, and structure definitions */
 typedef uint8_t bool;
+
+typedef enum {
+	MODULE_TEMP,
+	MODULE_WATER,
+} sending_module_t;
 
 typedef struct {
 	int state;
