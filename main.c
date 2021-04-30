03c2a6d0f131c1f05878151493bdf3d21b1135d6
Corrina Del Greco <corrinadelgreco@gmail.com>
2021-04-30 09:59:41 -0400

    Update main.c

--- main.c
+++ main.c
@@ -9,7 +9,7 @@
 // req 6: "The system shall capture/record/display the system events and actions."
 // req 5: "The system shall use time-stamping for identifying time of events (both ticks and sec/nsec)."
 void record(char *fmt) {
-	printf("[%d ticks - %d secs] %s\n", TIME_TICKS, TIME_SECS, fmt); // TODO format nicely
+	printf("[%d ticks - %d secs] %s\n", TIME_TICKS, TIME_SECS, fmt);
 }
 
 int main(void) {
